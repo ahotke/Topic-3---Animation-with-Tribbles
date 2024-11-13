9917fd02314d4816fad23016621961000c42e514
Ashton Hotke <339741886@GoTVDSB.ca>
2024-11-13 09:10:35 -0500

    done

--- Topic 3/Game1.cs
+++ Topic 3/Game1.cs
@@ -6,8 +6,8 @@ namespace Topic_3
 {
     public class Game1 : Game
     {
-        Texture2D tribbleGreyTexture, tribbleBrownTexture, tribbleCreamTexture, tribbleOrangeTexture;
-        Rectangle tribbleGreyRect, tribbleBrownRect, tribbleCreamRect, tribbleOrangeRect, window;
+        Texture2D tribbleGreyTexture, tribbleBrownTexture, tribbleCreamTexture, tribbleOrangeTexture, backgroundTexture;
+        Rectangle tribbleGreyRect, tribbleBrownRect, tribbleCreamRect, tribbleOrangeRect, window, backgroundRect;
         Vector2 tribbleGreySpeed, tribbleBrownSpeed, tribbleCreamSpeed, tribbleOrangeSpeed;
 
         private GraphicsDeviceManager _graphics;
@@ -29,16 +29,19 @@ namespace Topic_3
             _graphics.PreferredBackBufferWidth = window.Width;
             _graphics.PreferredBackBufferHeight = window.Height;
             _graphics.ApplyChanges();
+
             //Tribble Rectangles
-            tribbleGreyRect = new Rectangle(300, 10, 100, 100);
+            backgroundRect = new Rectangle(0, 0, window.Width, window.Height);
+            tribbleGreyRect = new Rectangle(300, 100, 100, 100);
             tribbleBrownRect = new Rectangle(650, 120, 100, 100);
             tribbleCreamRect = new Rectangle(110, 230, 100, 100);
             tribbleOrangeRect = new Rectangle(425, 340, 100, 100);
+
             //Vectors for speeds
             tribbleGreySpeed = new Vector2(1, 3);
             tribbleBrownSpeed = new Vector2(8, 6);
-            tribbleCreamSpeed = new Vector2(3, 2);
-            tribbleOrangeSpeed = new Vector2(4, 5);
+            tribbleCreamSpeed = new Vector2(-3, 2);
+            tribbleOrangeSpeed = new Vector2(4, -5);
             base.Initialize();
            
             
@@ -54,6 +57,7 @@ namespace Topic_3
             tribbleBrownTexture = Content.Load<Texture2D>("tribbleBrown");
             tribbleCreamTexture = Content.Load<Texture2D>("tribbleCream");
             tribbleOrangeTexture = Content.Load<Texture2D>("tribbleOrange");
+            backgroundTexture = Content.Load<Texture2D>("StarTrek");
         }
 
         protected override void Update(GameTime gameTime)
@@ -64,31 +68,53 @@ namespace Topic_3
             // TODO: Add your update logic here
             tribbleGreyRect.X += (int)tribbleGreySpeed.X;
             tribbleGreyRect.Y += (int)tribbleGreySpeed.Y;
-            if (tribbleGreyRect.Right > window.Width || tribbleGreyRect.Left < 0)
+            if (tribbleGreyRect.Right >= window.Width || tribbleGreyRect.Left <= 0)
+            {
                 tribbleGreySpeed.X *= -1;
-            if (tribbleGreyRect.Bottom > window.Bottom || tribbleGreyRect.Top < 0) 
+            }
+
+            if (tribbleGreyRect.Bottom >= window.Bottom || tribbleGreyRect.Top <= 0)
+            {
                 tribbleGreySpeed.Y *= -1;
+            }
 
             tribbleBrownRect.X += (int)tribbleBrownSpeed.X;
             tribbleBrownRect.Y += (int)tribbleBrownSpeed.Y;
-            if (tribbleBrownRect.Right > window.Width || tribbleBrownRect.Left < 0)
+            if (tribbleBrownRect.Right >= window.Width || tribbleBrownRect.Left <= 0)
+            {
                 tribbleBrownSpeed.X *= -1;
-            if (tribbleBrownRect.Bottom > window.Bottom || tribbleBrownRect.Top < 0) 
+            }
+            if (tribbleBrownRect.Bottom >= window.Bottom || tribbleBrownRect.Top <= 0)
+            {
                 tribbleBrownSpeed.Y *= -1;
+
+            }
+        
 
             tribbleCreamRect.X += (int)tribbleCreamSpeed.X;
             tribbleCreamRect.Y += (int)tribbleCreamSpeed.Y;
-            if (tribbleCreamRect.Right > window.Width || tribbleCreamRect.Left < 0)
+
+            if (tribbleCreamRect.Right >= window.Width || tribbleCreamRect.Left <= 0)
+            {
                 tribbleCreamSpeed.X *= -1;
-            if (tribbleCreamRect.Bottom > window.Bottom || tribbleCreamRect.Top < 0) 
+
+            }
+            if (tribbleCreamRect.Bottom >= window.Bottom || tribbleCreamRect.Top <= 0)
+            {
                 tribbleCreamSpeed.Y *= -1;
+
+            }
 
             tribbleOrangeRect.X += (int)tribbleOrangeSpeed.X;
             tribbleOrangeRect.Y += (int)tribbleOrangeSpeed.Y;
-            if (tribbleOrangeRect.Right > window.Width || tribbleOrangeRect.Left < 0)
+            if (tribbleOrangeRect.Right >= window.Width || tribbleOrangeRect.Left <= 0)
+            {
                 tribbleOrangeSpeed.X *= -1;
-            if (tribbleOrangeRect.Bottom > window.Bottom || tribbleOrangeRect.Top < 0) 
+            }
+            if (tribbleOrangeRect.Bottom >= window.Bottom || tribbleOrangeRect.Top <= 0)
+            {
                 tribbleOrangeSpeed.Y *= -1;
+            }
 
             if (tribbleGreyRect.Intersects(tribbleBrownRect) || tribbleGreyRect.Intersects(tribbleCreamRect) || tribbleGreyRect.Intersects(tribbleOrangeRect))
             {
@@ -123,6 +149,8 @@ namespace Topic_3
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
+
+            _spriteBatch.Draw(backgroundTexture, backgroundRect, Color.White);
 
             _spriteBatch.Draw(tribbleGreyTexture, tribbleGreyRect, Color.White);
           
